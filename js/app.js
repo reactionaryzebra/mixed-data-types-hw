abb68f33711b38d183e3a8d3b33e22ad404bc9fd
reactionaryzebra <theSource@Justins-MacBook-Air.local>
2019-03-25 15:54:42 -0700

    Completed problem 1

---
+++ js/app.js
@@ -0,0 +1,25 @@
+//1
+const lightSwitch = {
+  isOn: true
+}
+const user = {
+  email: 'Justin@me.com'
+}
+const spaceship = {
+  hull: 'steel',
+  laserBlasters: 'strong',
+  tractorBeam: 'strong',
+  warpDrive: 'Model z'
+}
+const studentNames = ['Justin', 'Josh', 'Rojelio'];
+const students = [
+  {
+    name: 'Justin',
+    location: 'Classroom 3',
+    favoriteTv: ['The West Wing', 'South Park', 'Hannibal']
+  }, {
+    name: 'Levi',
+    location: 'Classroom 3',
+    favoriteTv: []
+  }
+]
