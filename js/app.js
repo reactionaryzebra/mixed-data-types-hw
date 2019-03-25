693c09dff2614d3e13b14eac0585f7b0cc05802b
reactionaryzebra <theSource@Justins-MacBook-Air.local>
2019-03-25 16:00:42 -0700

    crazyObject

--- js/app.js
+++ js/app.js
@@ -34,3 +34,41 @@ const justin = {
   favoriteDataType: 'objects'
 }
 justin.hobby
+
+//3
+const crazyObject = {
+  taco: [
+    {
+      meat: 'steak',
+      cheese: ['panela', 'queso', 'chihuahua']
+    },
+    {
+      meat: 'chicken',
+      salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]
+    },
+  ],
+  larry: {
+    nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+    quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+    characters: [
+      {
+        name: "Jeff",
+        occupation: "manager"
+      },
+      {
+        name: "funkhauser",
+        occupation: "tv dude"
+      },
+      {
+        name: "susie",
+        occupation: "jeffs wife",
+        favoriteHobby: "Swearing at Larry and Jeff"
+      },
+    ]
+  }
+}
+console.log(crazyObject.taco[1].salsa[5]);
+console.log(crazyObject.larry.quotes[0]);
+console.log(crazyObject.larry.characters[2].favoriteHobby);
+console.log(crazyObject.larry.nicknames[1]);
+console.log(crazyObject.larry.characters[1]);
