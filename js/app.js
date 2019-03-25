a30ad793c8bef41ad9f367c3046a7ce538d0d2c0
reactionaryzebra <theSource@Justins-MacBook-Air.local>
2019-03-25 16:02:50 -0700

    object-ception

--- js/app.js
+++ js/app.js
@@ -72,3 +72,24 @@ console.log(crazyObject.larry.quotes[0]);
 console.log(crazyObject.larry.characters[2].favoriteHobby);
 console.log(crazyObject.larry.nicknames[1]);
 console.log(crazyObject.larry.characters[1]);
+
+
+//4
+const inception = {
+   reality: {
+       dreamLayer1: {
+           dreamLayer2: {
+               dreamLayer3: {
+                   dreamLayer4: {
+                       dreamLayer5: {
+                           dreamLayer6: {
+                               limbo: "Joseph Gordon Levitt"
+                          }
+                       }
+                   }
+               }
+           }
+       }
+   }
+}
+inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null;
